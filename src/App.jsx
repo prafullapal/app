49c57c21b70e9027bf491021ef0c0c30d466195b
prafullapal <prafullpal18@gmail.com>
2024-08-26 15:07:45 +0530

    first commit with signup

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter,  RouterProvider } from 'react-router-dom'
+import Auth from './pages/auth'
+import Chat from './pages/chat'
+import Profile from './pages/profile'
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/auth",
+      element: <Auth />
+    },
+    {
+      path: '/chat',
+      element: <Chat/>
+
+    }, {
+      path: "/profile",
+      element: <Profile/>
+    }
+  ])
+  return (
+    <RouterProvider router={router}/>
+  )
+}
+
+export default App;
---
+++ src/pages/profile/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Profile() {
+  return (
+    <div className='h-[100vh] w-[100vw] flex items-center justify-center'>
+      <h1 className='text-black'>Profile</h1>  
+    </div>
+  )
+}
